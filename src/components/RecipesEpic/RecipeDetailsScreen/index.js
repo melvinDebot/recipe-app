14368410e1d54429c6ea76849a0d28184a35c59e
Melvin Debot <melvin@exceenis.com>
2020-12-18 14:07:56 +0100

    setup navigation

--- src/components/RecipesEpic/RecipeDetailsScreen/index.js
+++ src/components/RecipesEpic/RecipeDetailsScreen/index.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
-const RecipesDetailsScreen = () => {
+const RecipesDetailsScreen = ({navigation}) => {
   return (
     <View>
       <Text>RecipeDetailsScreen</Text>
+      <Button title="revenir" onPress={() => {
+        navigation.goBack()
+      }} />
     </View>
   );
 };
--- src/components/RecipesEpic/RecipeListScreen/index.js
+++ src/components/RecipesEpic/RecipeListScreen/index.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-const RecipesListScreen = () => {
+const RecipesListScreen = ({navigation}) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>RecipeListScreen</Text>
+      <Button onPress={() => {
+        navigation.navigate('RecipeDetails')
+      }} title="move" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
 
 export default RecipesListScreen;
