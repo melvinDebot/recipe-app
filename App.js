ef9f0ddf966331b43a485c8acb80b6648e608405
Melvin Debot <melvin@exceenis.com>
2020-12-18 14:00:29 +0100

    add navigation

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-
 import { Provider } from "react-redux";
 import store from "./src/redux/store";
+import { NavigationContainer } from "@react-navigation/native"
+import {createStackNavigator} from "@react-navigation/stack"
 
 import RecipesListScreen from "./src/components/RecipesEpic/RecipeListScreen";
 import RecipesDetailsScreen from "./src/components/RecipesEpic/RecipeDetailsScreen";
 
+const Stack = createStackNavigator()
+
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <RecipesListScreen />
-        <RecipesDetailsScreen />
-      </View>
+        <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="RecipeList" component={RecipesListScreen} />
+          <Stack.Screen name="RecipeDetails" component={RecipesDetailsScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
